c7824920c94295e577f780e284d041e9b1ca7aab
CakeDev4k <vnicolas874@gmail.com>
2024-08-27 08:13:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+localStorage.setItem("theme", )
+
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light-cat.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar-cat.png")
+  }
+}
